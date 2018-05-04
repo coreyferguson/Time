3f034c76fc81b716743ef5f7f9463318c7d0e144
Corey Ferguson <corey.t.ferguson@gmail.com>
2018-05-04 16:41:12 -0700

    user repository

---
+++ src/user/dao/userRepository.js
@@ -0,0 +1,49 @@
+
+const AWS = require('aws-sdk');
+AWS.config.update({ region: 'us-west-2' });
+
+class UserRepository {
+
+  constructor(options) {
+    options = options || {};
+    this._dynamodb = options.dynamodb || new AWS.DynamoDB();
+    this._usersTableName = options.usersTableName || process.env.usersTableName;
+  }
+
+  findOne(userId) {
+    console.info(`UserRepository.findOne(userId): ${userId}`);
+    return new Promise((resolve, reject) => {
+      this._dynamodb.getItem({
+        TableName: this._usersTableName,
+        Key: {
+          id: {
+            S: userId
+          }
+        }
+      }, (err, data) => {
+        if (err) reject(err);
+        else resolve(data);
+      });
+    }).then(user => {
+      return (Object.keys(user).length === 0) ? null : user;
+    });
+  }
+
+  save(user) {
+    console.info(`UserRepository.save(user): ${user.id}`);
+    return new Promise((resolve, reject) => {
+      this._dynamodb.putItem({
+        TableName: this._usersTableName,
+        Item: user,
+        ReturnConsumedCapacity: 'TOTAL'
+      }, (err, data) => {
+        if (err) reject(err);
+        else resolve(data);
+      });
+    });
+  }
+
+}
+
+module.exports = new UserRepository();
+module.exports.UserRepository = UserRepository;
